f763cb84fbb55642c3cd470891a81d36f95588fc
42yasuke <j.ralph1998@gmail.com>
2025-04-15 18:42:41 +0200

    openclassroom 2.4

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -5,22 +5,7 @@ from listings.models import Movie
 
 def hello(request):
     bands = Band.objects.all()
-    movies = Movie.objects.all()
-    return HttpResponse(f"""
-        <h1>Hello Django !</h1>
-        <p>Mes groupes préférés sont :<p>
-        <ul>
-            <li>{bands[0].name}</li>
-            <li>{bands[1].name}</li>
-            <li>{bands[2].name}</li>
-        </ul>
-        <p>Mes films préférés sont :<p>
-        <ul>
-            <li>{movies[0].title}</li>
-            <li>{movies[1].title}</li>
-            <li>{movies[2].title}</li>
-        </ul>
-        """)
+    return render(request, 'listings/hello.html', {'bands': bands})
 
 def about(request):
     return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
