f771df53a54cb52823e45fe79895c46b2c35fc48
42yasuke <j.ralph1998@gmail.com>
2025-04-16 20:51:16 +0200

    op part 4.1

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -3,9 +3,15 @@ from django.shortcuts import render
 from listings.models import Band
 from listings.models import Movie
 
-def hello(request):
+def band_list(request):
     bands = Band.objects.all()
-    return render(request, 'listings/hello.html', {'bands': bands})
+    return render(request, 'listings/band_list', {'bands': bands})
+
+def band_detail(request, id):
+  band = Band.objects.get(id=id)  # nous insérons cette ligne pour obtenir le Band avec cet id
+  return render(request,
+          'listings/band_detail.html',
+          {'band': band}) # nous mettons à jour cette ligne pour passer le groupe au gabarit
 
 def about(request):
     return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
